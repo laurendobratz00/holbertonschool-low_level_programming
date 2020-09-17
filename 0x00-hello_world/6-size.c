ef4058945e396abbb9315900216df00f3b42dd2a
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-17 13:45:20 +0000

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,15 @@
 #include <stdio.h>
 int main(void)
 {
-	int x;
-	long int y;
-	char a;
+	int i;
+	long int l;
+	char c;
 	float f;
 	long long int L;
-printf("%lu\n", sizeof(int));
-printf("%lu\n", sizeof(long int));
-printf("%lu\n", sizeof(char));
-printf("%lu\n", sizeof(float));
-printf("%lu\n", sizeof(long long int));
+printf("%lu\n", sizeof(i));
+printf("%lu\n", sizeof(l));
+printf("%lu\n", sizeof(c));
+printf("%lu\n", sizeof(f));
+printf("%lu\n", sizeof(L));
 return (0);
 }

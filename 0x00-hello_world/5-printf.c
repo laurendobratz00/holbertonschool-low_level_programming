a1bd7b01653f9506a3d91a518ac64b3f173e5e58
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-17 02:36:07 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,12 @@
+/**
+* main- void
+* description: prints "with proper grammar, but the outcome is a piece of art,"
+* Return: 0
+*/
 
+#include <stdio.h>
+int main(void)
+{
+        printf(with proper grammar, but the outcome is a piece of art,);
+        return (0);
+}

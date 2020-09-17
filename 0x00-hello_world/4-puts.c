405be594856b23afba7778a7a8092d79f69a5bbf
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-17 02:21:25 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
-/* Printing a line without printf */
+/**
+* main- void
+* description: prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 
 #include <stdio.h>
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }

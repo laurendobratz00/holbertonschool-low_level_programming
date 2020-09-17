47a05e81dec6be686c4ecc60b02bbba8f8510e86
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-17 03:15:27 +0000

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+/**
+* main- void
+* description: prints string                           
+* Return: 0
+*/
+#include <stdio.h>
+int main(void)
+{
+printf("Size of a char: %c\n");
+printf("Size of and int: %d\n");
+printf("Size of a long int: %ld\n");
+printf("Size of a long long int: %lld\n");
+printf("Size of a float: %f\n");
+return (0);

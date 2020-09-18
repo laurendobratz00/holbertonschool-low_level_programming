9c503cc752a936079746f5a78c5f45bd203d0e0a
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-18 18:52:21 +0000

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- void
+ * description: Print single digit numbers
+ * Return: 0
+*/
+
+int main(void)
+{
+char i = 0;
+
+for (i = '0' ; i <= '9' ; i++)
+	{
+putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

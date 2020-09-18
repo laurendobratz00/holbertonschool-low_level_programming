7d3792e49978a2155e9b1cea6e45b0f3db7223b3
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-18 15:27:27 +0000

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+/**
+ * main- void
+ * description: Print upper and lowercase letters
+ * Return: 0
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+char a;
+char x = '\n';
+
+for (a = 'a' ; a <= 'z' ; a++)
+{
+putchar(a);
+}
+
+for (a = 'A' ; a <= 'Z' ; a++)
+{
+	putchar(a);
+}
+putchar(x);
+
+
+return (0);
+}

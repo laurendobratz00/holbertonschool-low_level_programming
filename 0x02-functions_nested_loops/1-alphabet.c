eec0b87cb621bcb80cbf80eaf9add6526ad3fa3d
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-21 21:36:40 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <holberton.h>
+
+/**
+ *
+ *
+ *
+ */
+
+void print_alphabet(void);
+
+{
+	char a;
+	char x = '\n';
+	for(a = 'a' ; a <= 'z' ; a++)
+	{putchar(a);
+	}
+	putchar(x);
+	return (0);
+}

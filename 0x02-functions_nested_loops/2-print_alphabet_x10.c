461d79f8bf55ccd3864a3db543f143a95ea9dea9
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-21 23:32:43 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <holberton.h>
+
+/**
+ *
+ *
+ *
+ */
+
+void print_alphabet_x10(void);
+
+{
+	char a;
+	int i;
+
+	for(a = 'a' ; a <= 'z' ; a++)
+		for(int i = 0; i < 10; i++)
+		{
+			putchar(i)
+				}
+{
+		putchar(a)
+			}

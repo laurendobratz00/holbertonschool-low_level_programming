db76a43621b8091eaa2428feeee2da93c79ac0b0
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-22 22:55:01 +0000

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+
+/*
+ * _abs(int) - printing absolute values
+ * description: printing absolute values
+ * @n: a number that prints the absolute value
+ * Return: 0
+ */
+
+int _abs(int);
+{
+	int x = abs(n);
+
+	if (n >= 0)
+	{
+		_putchar(n);
+	}
+	else
+	return (0);
+}

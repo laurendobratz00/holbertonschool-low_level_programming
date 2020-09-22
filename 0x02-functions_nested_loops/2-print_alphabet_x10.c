5d0fc20d29e6c34c741552a28522030b84c57cca
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-22 13:31:55 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,9 +13,9 @@ void print_alphabet_x10(void)
 	char a;
 	int i;
 
-	for(i = '0'; i <= '9' ; i++)
+	for (i = '0'; i <= '9' ; i++)
 	{
-		for(a = 'a' ; a <= 'z' ; a++)
+		for (a = 'a' ; a <= 'z' ; a++)
 			_putchar(a);
 		_putchar('\n');
 	}

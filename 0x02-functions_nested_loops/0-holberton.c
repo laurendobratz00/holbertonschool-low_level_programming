ebe92b0325c9b5123ee9a0907a03336cb90611d0
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-22 03:12:55 +0000

    0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
+#include "holberton.h"
 /**
- * main- void
- * description: Print Holberton
- * Return: 0
- */
+* main- void
+* desription: print alphabet
+* Return: 0
+*/
 
 int main(void)
 {
-	echo Holberton
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
 }

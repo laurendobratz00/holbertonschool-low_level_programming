dc14f6346863ff48054a283edded14cd7c72b49b
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-23 01:20:59 +0000

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+        int i;
+
+        i = 0;
+/**
+ * main - void
+ * desription: change i from 98 to 0
+ * Return: 0
+ */
+        positive_or_negative(i);
+
+        return (0);
+}

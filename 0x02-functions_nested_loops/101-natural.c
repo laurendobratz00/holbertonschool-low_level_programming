ce8be84d39e702fd0940634b5143ef1846865464
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-23 02:21:17 +0000

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,16 +6,17 @@
  *
  */
 
-int main (void)
-{
+int main ( void ) {
 
 	int sum = 0;
+	int cnt = 6;
 
 	for (int i = 0; i < 1024; i += 5) {
 		sum += i;
 	}
 	for (int i = 0; i < 1024; i += 3) {
-		if (i % 5) sum += i;
+		if (--cnt == 0) cnt = 5;
+		else sum += i;
 	}
 	printf("%d\n", sum);
 	return 0;

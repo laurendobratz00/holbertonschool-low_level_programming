a1a4f8caf42ccd69e8dea308173d8ddfa63a2f96
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-25 18:54:48 +0000

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,11 +10,29 @@
 void printnumber(int n)
 
 {
-	int n;
-
-	printf("n %d", n);
-	printf("\n");
-	putchar(n);
-
-	return 0;
+	if (n < 0)
+	{
+		n = n * -1;
+		_putchar('-');
+	}
+	if (n >= 0 && n <= 9)
+		_putchar((n % 10) + '0');
+	else if (n >= 10 && n <= 99)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n >= 100 && n <= 999)
+	{
+		_putchar((n / 100) + '0');
+		_putchar('0' + ((n / 10) % 10));
+		_putchar((n % 10) + '0');
+	}
+	else
+	{
+		_putchar((n / 1000) + '0');
+		_putchar(((n / 100) % 10) + '0');
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
 }

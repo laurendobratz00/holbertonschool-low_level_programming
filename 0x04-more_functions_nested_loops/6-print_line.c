ce414c7131330fee78d3ebc41a658c854212a409
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-25 14:47:05 +0000

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,16 @@
-#include <stdio.h>
 #include "holberton.h"
 
 /**
  * print_line - print straight line
  * description: print straight line
- *
+ * @n: character
  */
 
 void print_line(int n)
 {
 	int ln;
 
-	if (n > 0)
+	if (ln > 0)
 	{
 		for (ln = 0 ; ln <= n ; ln++)
 			_putchar('_');

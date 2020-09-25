deb3ec8b192ff4d98ce179defb627e3fc53f37ec
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-25 15:43:58 +0000

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,16 +12,17 @@ void print_diagonal(int n)
 	int ln, space;
 
 	for (ln = 0; ln < n; ln++)
-		if (n > 1)
 	{
-		for (space = 0; space <= ln; space++)
+		if (n > 1)
 		{
+			for (space = 0; space <= ln; space++)
+			{
 			_putchar(' ');
-		}
+			}
 		_putchar('\\');
 		_putchar('\n');
+		}
 	}
-}
 	if (n <= 0)
 	{
 	_putchar('\n');

041ccf4a6dc26fd2518e12398fe88ffe6b4e28e1
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-26 19:21:01 +0000

    debugging #2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,18 +5,18 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
- * Return: largest number
+ * Return: 0
  */
 
 int largest_number(int a, int b, int c)
-{
-	int largest;
 
-	if (a >= b && a >= c)
+{
+
+	if (a > b && a > c)
 	{
-		return (a);
+		return ('a');
 	}
-	else if (b >= a && b >= c)
+	else if (b > a && b > c)
 	{
 		return (b);
 	}

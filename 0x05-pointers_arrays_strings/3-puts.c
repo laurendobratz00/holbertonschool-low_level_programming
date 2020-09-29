20efa9f3de9973c489c252092eeafb387eb6daf9
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-29 15:20:18 +0000

    3-puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,9 +1,19 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * _puts - print string
+ * description: print string
+ * @str: string
+ * Return: void
  */
 
-void _puts(char *str);
+void _puts(char *str)
+
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+		_putchar(str[a]);
+
+	_putchar('\n');
+}

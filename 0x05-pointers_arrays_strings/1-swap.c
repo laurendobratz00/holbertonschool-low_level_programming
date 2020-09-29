a4702dbd1efb401d9c9ec1ff6797ca10b75d4b17
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-29 00:45:01 +0000

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps two variables
+ * @a: number 1
+ * @b: number 2
+ * description: swaps two variables
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *b;
+	*b = *a;
+	*a = temp;

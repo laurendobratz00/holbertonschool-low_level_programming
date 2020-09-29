4d5f40ba747dde0dee610652372c186e9653d7c2
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-29 00:33:41 +0000

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - change parameter
+ * description: change parameter
+ * @n: number 98
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}

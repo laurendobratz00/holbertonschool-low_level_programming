f5e6c82d04ff04c4e80458f96662e75f1ea4958f
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-29 14:13:35 +0000

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _strlen - return the length of a string
+ * description: return the length of a string
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int len;
+	char s;
+
+	while (s[len] != '\0')
+		return (len);
+}

425c9d85fc67e23d694e9cdfcd44e79787d9c04f
Lauren Dobratz <laurendobratz@icloud.com>
2020-09-29 14:35:48 +0000

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,14 +3,17 @@
 /**
  * _strlen - return the length of a string
  * description: return the length of a string
+ * @s: string
  * Return: 0
  */
 
 int _strlen(char *s)
 {
 	int len;
-	char s;
+
+	len = 0;
 
 	while (s[len] != '\0')
+		len++;
 		return (len);
 }

6e3354501fa36c0a9e1e2f3629a6ab110637783d
Lauren Dobratz <laurendobratz@icloud.com>
2020-10-02 18:54:08 +0000

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverse array
+ * description: reverse array
+ * @a: integer
+ * @n: integer
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int *ptr = a;
+	int tmp;
+	n -= 1;
+
+	for (i = 0; i < n; i++)
+		ptr++;
+	for (i = 0; i < n; i++)
+	{
+		tmp = *a;
+		*a = *ptr;
+		*ptr = tmp;
+		ptr--;
+		a++;
+	}
+}

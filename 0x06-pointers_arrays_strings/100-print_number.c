1eff23326583bbdd111ca93fe36e3d04d29a3da2
Lauren Dobratz <laurendobratz@icloud.com>
2020-10-02 01:50:55 +0000

    100-print_number.c

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_number - print integer
+ * description: print integer
+ * @n: integer
+ * Return: 0
+ */
+
+void print_number(int n)
+
+{
+char a;
+int b;
+char c;
+
+if (n < 0)
+{
+	_putchar('-');
+	a = ('0' - (n % 10));
+	n /= -10;
+}
+else
+{
+	a = ((n % 10) + '0');
+	n /= 10;
+}
+b = 0;
+while (n > 0)
+{
+	b = b * 10 + (n % 10);
+	n /= 10;
+}
+while (b > 0)
+{
+	c = ((b % 10) + '0');
+	_putchar(c);
+	b /= 10;
+}
+_putchar(a);
+}

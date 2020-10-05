477a9a42d95734ccdd0d8a7f70268c0d3f142709
Lauren Dobratz <laurendobratz@icloud.com>
2020-10-05 16:29:51 +0000

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,21 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
- *
+ * _memset - fill memory with constant byte
+ * description: fill memory with constant byte
+ * @s: character
+ * @b: character
+ * @n: unsigned integer
+ * Return: s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned char *p = s;
-
 	while (n > 0)
-		*p = b;
-	p++;
+	{
+	*s = b;
+	s++;
 	n--;
+	}
 	return (s);
 }

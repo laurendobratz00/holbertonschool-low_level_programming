3e1740003d453e53ed53a8f6105c774e180a861a
Lauren Dobratz <laurendobratz@icloud.com>
2020-10-05 15:58:51 +0000

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned char *p = s;
+	while (n > 0)
+		*p = b;
+	p++;
+	n--;
+	return (s);
+}

cf22336c002cef1e26deb99a19c983252c349633
Lauren Dobratz <laurendobratz@icloud.com>
2020-10-06 01:00:29 +0000

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -17,9 +17,11 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (haystack[i] == needle[j])
-				return (needle);
+			if (haystack[i + j] != needle[j])
+				break;
 		}
+		if (needle[j] == '\0')
+			return (haystack + i);
 	}
 	return (NULL);
 }

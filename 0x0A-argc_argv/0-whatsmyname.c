9f9f99a7e9092b2148610bcc818a4f77e96fb5c2
Lauren Dobratz <laurendobratz@icloud.com>
2020-10-11 21:35:10 +0000

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,15 +2,21 @@
 #include <stdio.h>
 
 /**
- * program_name - print program name
+ * main - print program name
  * description: print program name
  * @argc: integer
  * @argv: character
- * Return: 0
+ * Return: void
  */
 
-int program_name(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("program's name is: %s\n", argv[0]);
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s ", argv[i]);
+	}
+	printf("\n");
 	return (0);
 }
--- 0x0A-argc_argv/holberton.h
+++ 0x0A-argc_argv/holberton.h
@@ -1,7 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-int program_name(int argc, char *argv[]);
+int main(int argc, char *argv[]);
 int _putchar(char c);
 
 #endif

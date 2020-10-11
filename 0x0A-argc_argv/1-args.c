b94208743e21dd941755609f270b3f482355e7fd
Lauren Dobratz <laurendobratz@icloud.com>
2020-10-11 22:29:46 +0000

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print number of arguments
+ * description: print number of arguments
+ * @argc: integer
+ * @argv: void
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	(void)argv;
+	printf("argc = %d\n", argc);
+	return (0);
+}

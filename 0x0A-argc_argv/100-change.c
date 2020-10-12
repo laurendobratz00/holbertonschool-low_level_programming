7e5aabda1aa903567dd605ccc149a7abb4448ce0
Lauren Dobratz <laurendobratz@icloud.com>
2020-10-12 21:10:00 +0000

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,54 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - print coins
+ * description: print coins
+ * @argc: integer
+ * @argv: character
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+int change;
+int coin = 0;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+change = atoi(argv[1]);
+if (change < 0)
+printf("0\n");
+while (change >= 25)
+{
+	change -= 25;
+	coin++;
+}
+while (change >= 10)
+{
+	change -= 10;
+	coin++;
+}
+while (change >= 5)
+{
+	change -= 5;
+	coin++;
+}
+while (change >= 2)
+{
+	change -= 2;
+	coin++;
+}
+while (change >= 1)
+{
+	change -= 1;
+	coin++;
+}
+printf("%d", coin);
+return (0);
+}

c0e26c9abf5eff9c44ab01fa0b428dcd91e8b2e0
Lauren Dobratz <laurendobratz@icloud.com>
2020-10-14 16:29:23 +0000

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,7 +19,7 @@ char *_strdup(char *str)
 
 	for (len = 0; str[len] != '\0'; len++)
 		;
-	duplicate = malloc((len + 1) * sizeof(char))
+	duplicate = malloc((len + 1) * sizeof(char));
 		if (duplicate == NULL)
 			return (NULL);
 	for (i = 0; i <= len; i++)

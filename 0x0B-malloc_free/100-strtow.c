409b2df7557e1b3e86dc9687b929551f49f39174
Lauren Dobratz <laurendobratz@icloud.com>
2020-10-14 19:56:18 +0000

    100-strtow.c

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strtow - split string
+ * @str: character
+ * Return: yes
+ */
+
+char **strtow(char *str)
+{
+	str = str;
+}

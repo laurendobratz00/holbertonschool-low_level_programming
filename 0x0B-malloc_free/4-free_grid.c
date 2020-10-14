f2f9fa76e54c5f7c08bf46d55a4c3feceaa7c68e
Lauren Dobratz <laurendobratz@icloud.com>
2020-10-14 19:05:45 +0000

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - 2 dimensional grid
+ * @grid: integer
+ * @height: integer
+ * Return:
+ */
+
+void free_grid(int **grid, int height)
+{
+	int x;
+
+	for (x = 0; x < height; x++)
+	{
+		free(grid[x]);
+		free(grid);
+	}
+	free(grid);
+}

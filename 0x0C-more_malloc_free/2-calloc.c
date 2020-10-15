4a69ee55d86fc4367c43e5e37083bd50f8f9970a
Lauren Dobratz <laurendobratz@icloud.com>
2020-10-15 20:02:40 +0000

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	char *p;
 
-	p = malloc(size * nmemb);
+	p = malloc(sizeof(char) * (size * nmemb));
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);

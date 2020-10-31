470b21a622a211b46786e4ec20c8b6046701ad3e
laurendobratz00 <laurendobratz@icloud.com>
2020-10-31 04:00:39 +0000

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * before_main - prints something before main file is executed
+ * Return: void
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}

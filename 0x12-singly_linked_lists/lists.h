4cc3155edd4d12350d024d1b1d7effedfb7b75d8
laurendobratz00 <laurendobratz@icloud.com>
2020-10-31 15:23:24 +0000

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,5 +25,6 @@ int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 void before_main() __attribute__((constructor));
+list_t *add_node(list_t **head, const char *str);
 
 #endif

8c6a5a6453267164546d74923952c9f653f6a702
laurendobratz00 <laurendobratz@icloud.com>
2020-10-31 04:14:42 +0000

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,9 @@
  * Return: void
  */
 
+void before_main() __attribute__((constructor));
+void after_main() __attribute__((destructor));
+
 void before_main()
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");

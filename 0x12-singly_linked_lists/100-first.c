cb2795d3925784c3f570bd1c0c90174f5a46e200
laurendobratz00 <laurendobratz@icloud.com>
2020-11-02 20:37:08 +0000

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,15 +3,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+void print_b4_main(void)__attribute__((constructor));
+
 /**
- * before_main - prints something before main file is executed
+ * print_b4_main - prints before main is executed
  * Return: void
  */
 
-void before_main(void) __attribute__((constructor));
-void after_main(void) __attribute__((destructor));
-
-void before_main(void)
+void print_b4_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");

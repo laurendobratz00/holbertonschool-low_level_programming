d5aaac80ae07374925889bd40b76e001d269463e
laurendobratz00 <laurendobratz@icloud.com>
2020-11-03 16:38:45 +0000

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_listint2 - function that frees a singly linked list
+ * @head: head of the list
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+	listint_t *prev = *head;
+
+	while (prev != NULL)
+	{
+		tmp = prev->next;
+		free(prev);
+		prev = tmp;
+	}
+	*head = NULL;
+}

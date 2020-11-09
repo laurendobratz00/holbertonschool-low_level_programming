2ab9e61d773b68c9034fadcde366e9e41dc2c856
laurendobratz00 <laurendobratz@icloud.com>
2020-11-09 02:01:13 +0000

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,10 +12,10 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-        if (index > 64 || !n)
-                return (-1);
-        if (n == NULL)
-                return (-1);
-        *n = *n & (~(1 << index));
-        return (1);
+	if (index > 64 || !n)
+		return (-1);
+	if (n == NULL)
+		return (-1);
+	*n = *n & (~(1 << index));
+	return (1);
 }

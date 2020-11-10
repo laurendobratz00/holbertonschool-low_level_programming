1208ea3d7504e042015d29a89c640d3086470cc7
laurendobratz00 <laurendobratz@icloud.com>
2020-11-10 16:41:00 +0000

    Add include

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,6 +21,7 @@ int _strlen(char *s)
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "holberton.h"
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -42,7 +43,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);)
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fd == -1)
 	{
 		close(fd);

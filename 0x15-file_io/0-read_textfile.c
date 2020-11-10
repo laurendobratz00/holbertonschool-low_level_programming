be7c8c9f6f4498d69abb66cd12ebc59c27be3999
laurendobratz00 <laurendobratz@icloud.com>
2020-11-10 00:58:35 +0000

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	ssize_t rd;
+	ssize_t wr;
+
+	if (filename == NULL)
+		return (0);
+/* READ */
+	fd = open(filename, O_RDONLY);
+	if (fd == 1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+
+	if (buf == NULL)
+		return (0);
+
+	rd = read(fd, buf, letters);
+
+	if (rd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
+/* WRITE */
+	wr = write(STDOUT_FILENO, buf, rd);
+
+	if (wr == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	close(fd);
+	return (wr);
+}

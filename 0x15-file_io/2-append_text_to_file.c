49fdb97ac1ac8268988b8d9140a0348504e7b0a1
laurendobratz00 <laurendobratz@icloud.com>
2020-11-10 19:28:42 +0000

    Move if statement

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,16 +25,15 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
+	if (text_content == '\0')
+		return (1);
 	if (fd == -1)
 	{
 		return (-1);
 	}
 	for (i = 0; text_content[i] != '\0'; i++)
 		continue;
-	if (text_content == NULL)
-	{
-		return (1);
-	}
+
 	wr = write(fd, text_content, i);
 	if (wr == -1)
 	{

e6a3ba458df9fd7c5bdfab415c6bde315c0d77f4
laurendobratz00 <laurendobratz@icloud.com>
2020-11-10 17:27:43 +0000

    Fix close statements

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -38,11 +38,10 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	}
 	wr = write(fd, text_content, i);
+	close(fd);
 	if (wr == -1)
 	{
-		close(fd);
 		return (-1);
 	}
-	close(fd);
 	return (1);
 }

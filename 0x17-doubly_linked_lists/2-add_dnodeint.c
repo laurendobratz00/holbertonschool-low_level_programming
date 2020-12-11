57889f46697a4a50d57c4a31806e6a374eb216ed
laurendobratz00 <laurendobratz@icloud.com>
2020-12-11 16:42:17 +0000

    add file

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - add note to the front of a linked list
+ * @head: first node
+ * @n: int
+ * Return: address of the new element
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newnode;
+
+	newnode = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->n = n;
+	newnode->next = (*head);
+
+	(*head) = newnode;
+
+	return (newnode);
+}

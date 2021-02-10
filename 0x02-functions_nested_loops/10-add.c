ab45681be17fc974d4faf649cd8194f981b7c075
laurendobratz00 <laurendobratz@icloud.com>
2021-02-10 01:17:14 +0000

    add file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * add - adds two integers
+ * @a: int
+ * @b: int
+ * Return: x
+ */
+
+int add(int a, int b)
+{
+	int x;
+
+	x = a + b;
+	return (x);
+}

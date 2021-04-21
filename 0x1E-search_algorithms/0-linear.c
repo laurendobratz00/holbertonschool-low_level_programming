8120511b1accc6ffc6539ecd5fb6d850940e4359
laurendobratz00 <laurendobratz@icloud.com>
2021-04-21 21:01:26 +0000

    add changes

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,7 +18,7 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 	for (i = 0; i < size; i++)
 		if (array[i] == value)
-			printf("Value checked array[%li] = [%li]\n", i, size);
+			printf("Value checked array[%li] = [%d]\n", i, array[i]);
 			return (i);
 	return (-1);
 }
